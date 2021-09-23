fc8024c41c32c39e6447aa4e5e3396b747b8feed
nandhabn <nandhub9@gmail.com>
2021-09-23 19:15:42 +0530

    fixed contract migration query

--- contract/migrations/2_IPL_MomentsToken.js
+++ contract/migrations/2_IPL_MomentsToken.js
@@ -2,7 +2,7 @@
 const IPLM = artifacts.require("MomentSale");
 const IPLT = artifacts.require("IPLToken");
 
-const constants = require("../../src/utils/constans.json");
+const constants = require("../../iplt-frontend/src/utils/constans.json");
 const fs = require("fs");
 const path = require("path");
 
@@ -12,14 +12,20 @@ module.exports = async function (deployer) {
 
   let iplm = fs.readFileSync("./build/contracts/MomentSale.json");
   iplm = JSON.stringify(JSON.parse(iplm).abi);
-  fs.writeFileSync(path.resolve("../src/contracts/IPLMoments.json"), iplm);
+  fs.writeFileSync(
+    path.resolve("../iplt-frontend/src/contracts/IPLMoments.json"),
+    iplm
+  );
 
   let iplt = fs.readFileSync("./build/contracts/IPLToken.json");
   iplt = JSON.stringify(JSON.parse(iplt).abi);
-  fs.writeFileSync(path.resolve("../src/contracts/IPLToken.json"), iplt);
+  fs.writeFileSync(
+    path.resolve("../iplt-frontend/src/contracts/IPLToken.json"),
+    iplt
+  );
 
   fs.writeFileSync(
-    path.resolve("../src/utils/constans.json"),
+    path.resolve("../iplt-frontend/src/utils/constans.json"),
     JSON.stringify({
       ...constants,
       contractIds: { IPLMoments: IPLM.address, IPLToken: IPLT.address },
