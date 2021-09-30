e8cc5d58cda9f29a7d453569aee490f9bfcef6d8
arvindraj731 <arvindraj@calibraint.com>
2021-09-30 22:53:23 +0530

    Added Play Modal and ui changes

---
+++ iplt-frontend/src/app/container/MyCollections/MyCollections.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { DisplayMoments } from "../../components/DisplayMoments/DisplayMoments";
+
+export const MyCollections = () => {
+  return (
+    <div>
+      <DisplayMoments />
+    </div>
+  );
+};
